71cd27831647250b6a4e872a97ba0e5e602779d1
gitbluub <louis.auzuret@gmail.com>
2020-10-26 23:58:32 +0100

    added basic bubble sort and main file, need to add more sorts and tests

---
+++ sort.py
@@ -0,0 +1,9 @@
+from bubble_sort import bubble_sort
+
+arr = [64, 34, 25, 12, 22, 11, 90, -12] 
+  
+bubble_sort(arr) 
+  
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i]), 
